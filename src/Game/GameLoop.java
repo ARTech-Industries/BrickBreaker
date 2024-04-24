a581dc8d081eec6b38bae248929c367a5c3cacf6
BrianWanamaker <bcwanamaker@quinnipiac.edu>
2024-04-24 18:24:51 -0400

    added score to title

--- src/Game/GameLoop.java
+++ src/Game/GameLoop.java
@@ -1,6 +1,7 @@
 package Game;
 
 import java.awt.Graphics2D;
+import java.nio.file.WatchEvent;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -8,6 +9,7 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
 import javax.swing.JOptionPane;
 
+import GUIControls.Window;
 import Models.Ball;
 import Models.Brick;
 import Models.Paddle;
@@ -16,19 +18,19 @@ import Utils.Direction;
 import java.awt.Color;
 import java.awt.Font;
 
-
 public class GameLoop {
-	private Paddle paddle;
-	private Ball ball;
-	private Direction directionPressed;
-	private Brick[][] bricks;
-	private int totalNumberOfBricks;
-	private int numberOfBricksHit;
+    private Paddle paddle;
+    private Ball ball;
+    private Direction directionPressed;
+    private Brick[][] bricks;
+    private int totalNumberOfBricks;
+    private int numberOfBricksHit;
     private CollisionHandler collisionHandler;
     private boolean paused;
     private GamePanel gp;
-    
-    public GameLoop(GamePanel gp){
+    private int score;
+
+    public GameLoop(GamePanel gp) {
         this.gp = gp;
     }
 
@@ -37,52 +39,51 @@ public class GameLoop {
     public void setup() {
         // create game objects
         paddle = new Paddle();
-		ball = new Ball();
-		bricks = new Brick[6][6];
+        ball = new Ball();
+        bricks = new Brick[6][6];
         numberOfBricksHit = 0;
-		
-		int brickWidth = 120;
-		int brickHeight = 20;
-		int horizontalSpacing = 5;
-		int verticalSpacing = 5;
-		int spacingFromWallX = 20;
-		int spacingFromWallY = 5;
 
+        int brickWidth = 120;
+        int brickHeight = 20;
+        int horizontalSpacing = 5;
+        int verticalSpacing = 5;
+        int spacingFromWallX = 20;
+        int spacingFromWallY = 5;
 
-        try{
+        try {
             AudioInputStream audioIn = AudioSystem.getAudioInputStream(GameLoop.class.getResource("Truth.wav"));
             Clip clip = AudioSystem.getClip();
             clip.open(audioIn);
             FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
             gainControl.setValue(-15.0f);
             clip.start();
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-		
         // create brick for each slot available in 2D array
         totalNumberOfBricks = 0;
-		for (int i = 0; i < bricks.length; i++) {
-			for (int j = 0; j < bricks[i].length; j++) {
-				int brickXLocation = j * brickWidth + (j * horizontalSpacing) + spacingFromWallX;
-				int brickYLocation = i * brickHeight + (i * verticalSpacing) + spacingFromWallY;
-				bricks[i][j] = new Brick(brickXLocation, brickYLocation, brickWidth, brickHeight);
-				totalNumberOfBricks++;
-			}
-		}
+        for (int i = 0; i < bricks.length; i++) {
+            for (int j = 0; j < bricks[i].length; j++) {
+                int brickXLocation = j * brickWidth + (j * horizontalSpacing) + spacingFromWallX;
+                int brickYLocation = i * brickHeight + (i * verticalSpacing) + spacingFromWallY;
+                bricks[i][j] = new Brick(brickXLocation, brickYLocation, brickWidth, brickHeight);
+                totalNumberOfBricks++;
+            }
+        }
 
         // start paddle in middle of screen
         paddle.setXLocation(GamePanel.WIDTH / 2 - paddle.getWidth() / 2);
-		paddle.setYLocation(GamePanel.HEIGHT - paddle.getHeight() - 10);
+        paddle.setYLocation(GamePanel.HEIGHT - paddle.getHeight() - 10);
 
         // start ball on top of the paddle and center it
-		int paddleX2 = paddle.getXLocation() + paddle.getWidth();
-		int difference = paddleX2 - paddle.getXLocation();
-		ball.setXLocation(paddle.getXLocation() + (difference / 2) - (ball.getWidth() / 2));
-		ball.setYLocation(paddle.getYLocation() - ball.getHeight() - 5);
+        int paddleX2 = paddle.getXLocation() + paddle.getWidth();
+        int difference = paddleX2 - paddle.getXLocation();
+        ball.setXLocation(paddle.getXLocation() + (difference / 2) - (ball.getWidth() / 2));
+        ball.setYLocation(paddle.getYLocation() - ball.getHeight() - 5);
 
-        // create collision handler instance to use throughout program for detecting collisions such as the ball hitting the paddle or the ball hitting a brick
+        // create collision handler instance to use throughout program for detecting
+        // collisions such as the ball hitting the paddle or the ball hitting a brick
         collisionHandler = new CollisionHandler(ball, paddle, bricks);
 
         paused = false; // start unpaused by default
@@ -91,17 +92,17 @@ public class GameLoop {
     // called every frame while the game runs
     public void update() {
         // only runs game behavior when the game is unpaused
-        if(!paused) {
+        if (!paused) {
             // if a direction is being pressed (either left or right), move paddle
-            // if when paddle is moved, it runs into the ball, it pushes the ball over a little bit
+            // if when paddle is moved, it runs into the ball, it pushes the ball over a
+            // little bit
             if (directionPressed != null) {
                 if (directionPressed == Direction.LEFT) {
                     paddle.moveLeft();
                     if (ball.intersects(paddle)) {
                         ball.setXLocation(paddle.getXLocation() - ball.getWidth());
                     }
-                } 
-                else if (directionPressed == Direction.RIGHT) {
+                } else if (directionPressed == Direction.RIGHT) {
                     paddle.moveRight();
                     if (ball.intersects(paddle)) {
                         ball.setXLocation(paddle.getXLocation() + paddle.getWidth());
@@ -112,32 +113,37 @@ public class GameLoop {
             // move ball either up or down based on its current y direction
             ball.moveY();
 
-            // check if ball hit ceiling -- if so, reverse its direction so it starts to move downwards
+            // check if ball hit ceiling -- if so, reverse its direction so it starts to
+            // move downwards
             // this makes it appear as if the ball is bouncing off the ceiling
             if (ball.getYDirection() == Direction.UP && ball.getYLocation() < 0) {
                 ball.setYDirection(Direction.DOWN);
                 ball.setYLocation(0);
-            } 
-            
+            }
+
             // check if ball has collided with paddle while moving downwards
             collisionHandler.paddleBallCollisionHandler(Axis.Y);
-            
-            // check if ball has collided with any of the bricks after it has moved up or down
+
+            // check if ball has collided with any of the bricks after it has moved up or
+            // down
             boolean isBrickYCollision = collisionHandler.brickBallCollisionHandler(Axis.Y);
             if (isBrickYCollision) {
+                score++;
                 numberOfBricksHit++;
+                Window.setTitle("Score : " + score);
             }
 
             // move ball either left or right based on its current x direction
             ball.moveX();
-            
-            // check if ball hit either the left or right walls -- if so, reverse its direction
+
+            // check if ball hit either the left or right walls -- if so, reverse its
+            // direction
             // this makes it appear as if the ball is bouncing off the walls
             if (ball.getXDirection() == Direction.LEFT && ball.getXLocation() < 0) {
                 ball.setXDirection(Direction.RIGHT);
                 ball.setXLocation(0);
-            } 
-            else if (ball.getXDirection() == Direction.RIGHT && ball.getXLocation() + ball.getWidth() > GamePanel.WIDTH) { 
+            } else if (ball.getXDirection() == Direction.RIGHT
+                    && ball.getXLocation() + ball.getWidth() > GamePanel.WIDTH) {
                 ball.setXDirection(Direction.LEFT);
                 ball.setXLocation(GamePanel.WIDTH - ball.getWidth());
             }
@@ -145,22 +151,25 @@ public class GameLoop {
             // check if ball has collided with paddle while moving left or right
             // very rare to actually happen but here just in case :)
             collisionHandler.paddleBallCollisionHandler(Axis.X);
-            
-            // check if ball has collided with any of the bricks after it has moved left or right
+
+            // check if ball has collided with any of the bricks after it has moved left or
+            // right
             boolean isBrickXCollision = collisionHandler.brickBallCollisionHandler(Axis.X);
             if (isBrickXCollision) {
+                score++;
                 numberOfBricksHit++;
+                Window.setTitle("Score : " + score);
             }
 
             // if all bricks hit, game over, player wins
             if (allBricksHit()) {
                 System.out.println("Player has completed this level.");
 
-                JOptionPane.showMessageDialog(gp,"You win!");
+                JOptionPane.showMessageDialog(gp, "You win!");
 
                 System.exit(1);
             }
-            
+
             // if ball hits bottom of the screen, game over, player loses
             if (ball.getYLocation() + ball.getHeight() >= GamePanel.HEIGHT) {
                 System.exit(0);
@@ -173,33 +182,35 @@ public class GameLoop {
     public void draw(Graphics2D graphics) {
         // draw paddle and ball rectangle graphics to screen
         paddle.draw(graphics);
-		ball.draw(graphics);
-        
+        ball.draw(graphics);
+
         // draw each brick to the screen that hasn't yet been hit
-		for (Brick[] brickArray : bricks) {
-			for (Brick brick : brickArray) {
-				if (!brick.getIsHit()) {
-					brick.draw(graphics);
-				}
-			}
-		}
-         // Display the score
-    graphics.setColor(Color.BLACK); // Set the color for the score text
-    graphics.setFont(new Font("Arial", Font.BOLD, 20)); // Set the font for the score text
-    graphics.drawString("Score: " + numberOfBricksHit, 10, GamePanel.HEIGHT - 50); // Draw the score text in the bottom left 
+        for (Brick[] brickArray : bricks) {
+            for (Brick brick : brickArray) {
+                if (!brick.getIsHit()) {
+                    brick.draw(graphics);
+                }
+            }
+        }
+        // Display the score
+        graphics.setColor(Color.BLACK); // Set the color for the score text
+        graphics.setFont(new Font("Arial", Font.BOLD, 20)); // Set the font for the score text
+        graphics.drawString("Score: " + numberOfBricksHit, 10, GamePanel.HEIGHT - 50); // Draw the score text in the
+                                                                                       // bottom left
 
     }
 
     // returns if all bricks have been hit yet or not
-	private boolean allBricksHit() {
-		return numberOfBricksHit == totalNumberOfBricks;
-	}
+    private boolean allBricksHit() {
+        return numberOfBricksHit == totalNumberOfBricks;
+    }
 
     public Direction getDirectionPressed() {
         return this.directionPressed;
     }
 
-    // GamePanel file uses this to tell GameLoop which direction was pressed (left or right)
+    // GamePanel file uses this to tell GameLoop which direction was pressed (left
+    // or right)
     public void setDirectionPressed(Direction directionPressed) {
         this.directionPressed = directionPressed;
     }
