89a0ab151b9cf0ad499bdacd877a1f536382c945
Garrett Goldberg <98359240+gegoldberg@users.noreply.github.com>
2024-04-24 17:58:36 -0400

    Fixed Brick Numbers

--- src/Game/GameLoop.java
+++ src/Game/GameLoop.java
@@ -30,7 +30,7 @@ public class GameLoop {
         // create game objects
         paddle = new Paddle();
 		ball = new Ball();
-		bricks = new Brick[1][2];
+		bricks = new Brick[3][6];
         numberOfBricksHit = 0;
 		
 		int brickWidth = 120;
